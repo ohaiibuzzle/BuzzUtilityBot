a5eb4a7b944dd729a36751de60fb169c83e8947e
OHaiiBuzzle <23693150+ohaiibuzzle@users.noreply.github.com>
2023-10-04 21:30:26 +0700

    fix: remove dead code

--- src/main.py
+++ src/main.py
@@ -68,26 +68,14 @@ intents.members = True  # pylint: disable=assigning-non-slot
 intents.message_content = True
 
 client = bridge.Bot(command_prefix=".", intents=intents)
+
 @client.event
 async def on_ready():
     logging.info("Logged in as {0.user}".format(client))
     game = discord.Game("in Buzzle's Box. Available on GitHub")
     await client.change_presence(status=discord.Status.online, activity=game)
 
-
-
 client.add_cog(AdminTools(client))
-
-# client.load_extension("pic_source.PictureSearch")
-# client.load_extension("sauce_find.SauceFinder")
-# client.load_extension("tf_image_processor.TFImage")
-# client.load_extension("utils.MessageUtils")
-# client.load_extension("utils.Welcome")
-# client.load_extension("utils.Birthday")
-# client.load_extension("utils.owo")
-# client.load_extension("music.Music")
-# client.load_extension("twitterwatch.TwitterWatcher")
-# client.load_extension("utils.nsfwRole")
 
 features = [
     "image_search.PictureSearch",
